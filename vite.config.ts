7b8ef9bf2aa35e95aae950fa1fad9b0397938715
TamilNangaiK <k.tamilpirai@gmail.com>
2026-04-05 23:40:39 +0530

    me1

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: "/edumarkkce/",
+  base: "/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
